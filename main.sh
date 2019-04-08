55fb82092f06a18e791abdbc05590c2a348a6ea0
suzusime <tamagawajousuiro+git@gmail.com>
2019-04-08 09:10:17 +0000

    add fzf

--- main.sh
+++ main.sh
@@ -8,3 +8,7 @@ $HOME/.homesick/repos/homeshick/bin/homeshick clone suzusime/nijo
 
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --no-fish --no-bash --completion --key-bindings --no-update-rc
