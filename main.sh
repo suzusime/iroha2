1a0225a6fbb6247f555b68352e18911f0c1473b1
suzusime <tamagawajousuiro+git@gmail.com>
2019-07-07 19:53:40 +0000

    update about fzf

--- main.sh
+++ main.sh
@@ -2,20 +2,22 @@
 
 # set env temporally
 XDG_CONFIG_HOME="$HOME/.config"
-mkdir -o $XDG_CONFIG_HOME
+mkdir -p $XDG_CONFIG_HOME
+mkdir -p $XDG_CONFIG_HOME/log
+
 
 # install homeshick
-git clone git://github.com/andsens/homeshick.git $XDG_CONFIG_HOME/homesick/repos/homeshick
+git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 
 # download the castle
-HOMESHICK_DIR=$XDG_CONFIG_HOME/homesick/repos/homeshick $XDG_CONFIG_HOME/homesick/repos/homeshick/bin/homeshick clone suzusime/sanjo
+$HOME/.homesick/repos/homeshick/bin/homeshick clone suzusime/sanjo
 
 # install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions $XDG_CONFIG_HOME/zsh/zsh-autosuggestions
 
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git $XDG_CONFIG_HOME/fzf
-$XDG_CONFIG_HOME/fzf/install --no-fish --no-bash --completion --key-bindings --no-update-rc
+$XDG_CONFIG_HOME/fzf/install --xdg --no-fish --no-bash --completion --key-bindings --no-update-rc
 
 # install spacemacs
 # git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
